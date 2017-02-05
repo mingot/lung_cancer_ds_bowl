cd6dd270e223fca76b962183cdb4cb2d0b0722de
Ricard Delgado <ricard.delgado@csem.ch>
2017-02-05 17:46:55 +0100

    Una mica de cleanup i nous TODOs

--- src/jm_pipeline_preproc.py
+++ src/jm_pipeline_preproc.py
@@ -6,45 +6,65 @@ from utils import preprocessing
 from utils import plotting
 import matplotlib.pyplot as plt
 
-# Reading patients
+# Define folder locations
 wp = os.environ['LUNG_PATH']
 TMP_FOLDER = wp + 'data/jm_tmp/'
-INPUT_FOLDER = wp + 'data/sample_images/'  # 'data/stage1/stage1/'
+INPUT_FOLDER = wp + 'data/stage1/'
+# INPUT_FOLDER = wp + 'data/sample_images/'
 OUTPUT_FOLDER = wp + 'data/stage1_proc/'
 PIPELINE = 'dsb'  # for filename
 
-patients = os.listdir(INPUT_FOLDER)
+# Execution parameters
+show_intermediate_images = True
+
+# Small code to get rid of .DS_Store
+patients = []
+for f in os.listdir(INPUT_FOLDER):
+    if not f.startswith('.'):
+        patients.append(f)
+
 patients.sort()
 
+# Main loop over the ensemble of teh database
 times = []
-
-for patid in patients:
+for pat_id in patients:
     
     n = time()
     
-    # Read
-    # patid = patients[10]
-    patient = reading.load_scan(INPUT_FOLDER + patid)
+    # Read the input image
+    patient = reading.load_scan(INPUT_FOLDER + pat_id)
     
     # From pixels to HU
     patient_pixels = preprocessing.get_pixels_hu(patient)
-    #patient_pixels.shape
-    #plt.imshow(patient_pixels[3])
-    #plt.hist(patient_pixels.flatten(), bins=80, color='c')
-    
+    if show_intermediate_images:
+        print("Shape of raw data\t", patient_pixels.shape)
+        plt.figure()
+        plt.imshow(patient_pixels[3])
+        plt.title('Raw pixel data')
+        plt.figure()
+        plt.hist(patient_pixels.flatten(), bins=80, color='c')
+        plt.title('Histogram')
+
     # Resampling
-    pix_resampled, spacing = preprocessing.resample(patient_pixels, patient, [1,1,1])  # slow
-    #print("Shape before resampling\t", patient_pixels.shape)
-    #print("Shape after resampling\t", pix_resampled.shape)
-    #plt.imshow(pix_resampled[50])
-    #plotting.plot_3d(pix_resampled, 400)
+    # TODO: Accelerate the resampling
+    pix_resampled, spacing = preprocessing.resample(patient_pixels, patient, [1, 1, 1])
+    if show_intermediate_images:
+        print("Shape after resampling\t", pix_resampled.shape)
+        plt.figure()
+        plt.imshow(pix_resampled[50])
+        plt.title('Resampled data')
+        # plt.figure()
+        # plotting.plot_3d(pix_resampled, 400)
     
     # Segment lungs
-    lung_mask = preprocessing.segment_lung_mask(pix_resampled, fill_lung_structures=True)  # TODO: expand the mask (as suggested in the kernel)
-    #lung_mask.shape
-    #plt.imshow(lung_mask[90])
-    #plotting.plot_3d(segmented_lungs_fill, 0)
-    #plotting.plot_3d(segmented_lungs_fill - segmented_lungs, 0)
+    lung_mask = preprocessing.segment_lung_mask(pix_resampled, fill_lung_structures=True)
+    # TODO: expand the mask (as suggested in the kernel)
+    if show_intermediate_images:
+        lung_mask.shape
+        plt.figure()
+        plt.imshow(lung_mask[90])
+        # plotting.plot_3d(segmented_lungs_fill, 0)
+        # plotting.plot_3d(segmented_lungs_fill - segmented_lungs, 0)
     
     # zero center and normalization
     pix = preprocessing.normalize(pix_resampled)
@@ -52,12 +72,14 @@ for patid in patients:
     
     # store output (processed lung and lung mask)
     output = np.stack((pix, lung_mask))
-    np.savez_compressed(OUTPUT_FOLDER + "%s_%s.npz" % (PIPELINE, patid), output)  # 10x compression over np.save (~400Mb vs 40Mg), but 10x slower  (~1.5s vs ~15s)
-    #np.save("prova.npy", output)
+    # TODO: The following line crashes if the output folder does not exist
+    np.savez_compressed(OUTPUT_FOLDER + "%s_%s.npz" % (PIPELINE, pat_id), output)  # 10x compression over np.save (~400Mb vs 40Mg), but 10x slower  (~1.5s vs ~15s)
+    # np.save("prova.npy", output)
     
     x = time()-n
     print 'Time: %s' % str(x)
     times.append(x)
 
-
 print 'Average time per image: %s' % str(np.mean(times))
+
+
