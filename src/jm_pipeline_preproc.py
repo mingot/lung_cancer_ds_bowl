cceae3f8a41f8f97bcf51551f1940713acad9f52
Gabriel Bernardino <gabriel.bernardino@upf.edu>
2017-02-05 19:36:14 +0100

    lidcs reading;

--- src/jm_pipeline_preproc.py
+++ src/jm_pipeline_preproc.py
@@ -1,3 +1,11 @@
+"""
+File for preprocessing the datasets. It gets as input the DCOMS path, and converts it to numpy.
+
+
+Datasets accepted:
+    -DSB
+"""
+
 import os
 import numpy as np
 from time import time
@@ -8,11 +16,24 @@ import matplotlib.pyplot as plt
 
 # Reading patients
 wp = os.environ['LUNG_PATH']
-TMP_FOLDER = wp + 'data/jm_tmp/'
-INPUT_FOLDER = wp + 'data/sample_images/'  # 'data/stage1/stage1/'
-OUTPUT_FOLDER = wp + 'data/stage1_proc/'
+TMP_FOLDER = os.path.join(wp, 'data/jm_tmp/')
+INPUT_FOLDER = os.path.join(wp, 'data/sample_images/')  # 'data/stage1/stage1/'
+OUTPUT_FOLDER = os.path.join(wp, 'data/stage1_proc/')
 PIPELINE = 'dsb'  # for filename
 
+#Overwriting parameters by console
+for arg in sys.argv[1:]:
+    if arg.startswith('--input='):
+        INPUT_FOLDER = ''.join(arg.split('=')[1:])
+    elif arg.startswith('--output='):
+        OUTPUT_FOLDER = ''.join(arg.split('=')[1:])
+    elif arg.startswith('--tmp='):
+        TMP_FOLDER =''.join(arg.split('=')[1:])
+    elif arg.statswith('--pipeline='):
+        PIPELINE = ''.join(arg.split('=')[1:])
+    else:
+        print 'Unknown argument %s. Ignoring.' %arg
+        
 patients = os.listdir(INPUT_FOLDER)
 patients.sort()
 
@@ -24,7 +45,10 @@ for patid in patients:
     
     # Read
     # patid = patients[10]
-    patient = reading.load_scan(INPUT_FOLDER + patid)
+    if PIPELINE == 'dsb':
+        patient = reading.load_scan(INPUT_FOLDER + patid)
+    elif PIPELINE == 'lidc':
+        patient = reading.read_patient_lidc(INPUT_FOLDER + patid)
     
     # From pixels to HU
     patient_pixels = preprocessing.get_pixels_hu(patient)
@@ -38,13 +62,18 @@ for patid in patients:
     #print("Shape after resampling\t", pix_resampled.shape)
     #plt.imshow(pix_resampled[50])
     #plotting.plot_3d(pix_resampled, 400)
-    
+        
     # Segment lungs
     lung_mask = preprocessing.segment_lung_mask(pix_resampled, fill_lung_structures=True)  # TODO: expand the mask (as suggested in the kernel)
     #lung_mask.shape
     #plt.imshow(lung_mask[90])
     #plotting.plot_3d(segmented_lungs_fill, 0)
     #plotting.plot_3d(segmented_lungs_fill - segmented_lungs, 0)
+    
+    #Compute volume for sanity test
+    lung_volume_l = np.sum(lung_mask)/(100.**3)
+    if lung_volume_l < 2 or lung_volume_l > 10:
+        print 'Warning lung volume: %s out of physiological values. Double Check segmentation.' % patid 
     
     # zero center and normalization
     pix = preprocessing.normalize(pix_resampled)
