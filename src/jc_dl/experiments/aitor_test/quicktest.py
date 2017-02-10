9b95e2bd70729a51c26a328b0259e2b6e60bcd2b
aiorla <ortizdelatierroaitor@gmail.com>
2017-02-10 20:59:50 +0100

    Quick DL v0.3

--- src/jc_dl/experiments/aitor_test/quicktest.py
+++ src/jc_dl/experiments/aitor_test/quicktest.py
@@ -1,21 +1,19 @@
+import os
 import numpy as np
 from keras.optimizers import Adam
 from keras import backend as K
-import os
 from networks.unet import UNETArchitecture
-import time
 
 K.set_image_dim_ordering('th')
 smooth     = 1.
 
 # PARAMETERS
 # nb_epoch: # training epochs
-# batch_size: # PATIENTS in a batch
+# batch_size: aprox. # slices in a batch
 nb_epoch   = 1
 batch_size = 1
-input_path = '/home/aitor/test'
-#output_path = '/home/aiorla/test_output'
-model_path = '/home/aitor/lung_cancer_ds_bowl/models'
+input_path = '/mnt/hd2/preprocessed2'
+output_model_path = '/home/aitor/lung_cancer_ds_bowl/models'
 
 def load_patients(index,num_patients,filelist):
     X_batch, Y_batch = [], []
@@ -24,7 +22,6 @@ def load_patients(index,num_patients,filelist):
        for j in range(b.shape[1]):
            X_batch.append(b[0,j,:,:])
            Y_batch.append(b[2,j,:,:])
-       print(len(X_batch))
     return X_batch, Y_batch
 
 def dice_coef(y_true, y_pred):
@@ -50,18 +47,14 @@ model.compile(optimizer=Adam(lr=1.0e-5), loss=dice_coef_loss, metrics=[dice_coef
 for e in range(nb_epoch):
     print("epoch %d" % e)
     for i in range(0,len(file_list),batch_size):
-        X_batch, Y_batch = load_patients(i,batch_size,file_list)
-	print("pacient %d" % i)
-	for j in range(len(X_batch)):
-		X = np.expand_dims(np.expand_dims(X_batch[j],axis=0),axis=0)
-		Y = np.expand_dims(np.expand_dims(Y_batch[j],axis=0),axis=0)
-		model.train_on_batch(X,Y)
-		print("slice %d" % j)
-		
-        #X = np.expand_dims(np.asarray(X_batch),axis=1) #it needs an array of size (N,1,800,800)
-        #Y = np.expand_dims(np.asarray(Y_batch),axis=1) #it needs an array of size (N,1,800,800)
-        #model.train_on_batch(X, Y) # TOTRY
-    model.save(model_path+'/unet-'+str(e)+'.h5')
+        X_patients, Y_patients = load_patients(i,1,file_list)
+        print("pacient %d" % i)
+    	for j in range(0,len(X_batch),batch_size):
+    		X = np.expand_dims(np.asarray(X_patients[j:(j+batch_size-1)]),axis=1)
+    		Y = np.expand_dims(np.asarray(Y_patients[j:(j+batch_size-1)]),axis=1)
+    		model.train_on_batch(X,Y)
+    		print("slice %d" % j)
+    model.save(output_model_path+'/unet-'+str(e)+'.h5')
 
 # LOAD & PREDICT & SAVE TEST DATA
 # TODO...
