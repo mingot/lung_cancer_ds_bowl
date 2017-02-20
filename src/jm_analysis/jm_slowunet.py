bf465f5c275f2338034361fd7a506af28ca45095
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2017-02-20 14:03:02 +0100

    evaluation of DL

--- src/jm_analysis/jm_slowunet.py
+++ src/jm_analysis/jm_slowunet.py
@@ -1,7 +1,5 @@
-#   SLOWUNET
-#   - Go through all the dataset batch by batch
-#   - TODO: NOT train slices without "considerable" portion of segmented lung
-#   - TODO: Adapt to DL-Pipelines framework (when it is stable...)
+#   JM_SLOWUNET
+
 
 import os
 import numpy as np
@@ -10,18 +8,15 @@ from time import time
 from keras.optimizers import Adam
 from keras import backend as K
 from networks.unet import UNETArchitecture
-from datasets.basic_dataset import LunaNonEmptyMasked_SlicesDataset
 from experiments.jose_cordero_sample_experiment.experiments_utils import visualize_case #, dice_coef_loss
 from utils.tb_callback import TensorBoard
 
 K.set_image_dim_ordering('th')
 
 # PARAMETERS
-num_epoch = 10
-max_data_chunk = 2
-max_batch_size = 2
-prefixes_to_load = ['luna_']
-USE_EXISTING = True
+NUM_EPOCHS = 10
+BATCH_SIZE = 2
+USE_EXISTING = True  # load previous model to continue training
 
 
 ## paths
@@ -35,8 +30,7 @@ if not os.path.exists(logs_path):
 
 
 ## tensorboard logs
-#tb = keras.callbacks.TensorBoard(log_dir=logdir, histogram_freq=1, write_graph=False, write_images=True)
-tb = TensorBoard(log_dir=logs_path, histogram_freq=1, write_graph=False, write_images=False)
+tb = TensorBoard(log_dir=logs_path, histogram_freq=1, write_graph=False, write_images=False)  # replace keras.callbacks.TensorBoard
 
 
 ## model loading
@@ -51,7 +45,7 @@ model = arch.get_model()
 model.compile(optimizer=Adam(lr=1.0e-6), loss=dice_coef_loss, metrics=[dice_coef_loss])
 
 if USE_EXISTING:
-    model.load_weights(model_path + 'jm_slowunet_v3.hdf5')
+    model.load_weights(model_path + 'jm_slowunet_v4.hdf5')
 
 ## Load LUNA dataset
 normalize = lambda x: (x - np.mean(x))/np.std(x)
@@ -106,89 +100,81 @@ import random
 mylist = os.listdir(input_path)
 file_list = [g for g in mylist if g.startswith('luna_')]
 random.shuffle(file_list)
-print 'Creating test set...'
-X_test, Y_test = load_patients(file_list[-10:])
+
+# print 'Creating test set...'
+# X_test, Y_test = load_patients(file_list[-10:])
+# file_list = file_list[:-10]
 
 
-NUM_EPOCHS = 2
-print('Training...\n')
-model_checkpoint = keras.callbacks.ModelCheckpoint(model_path + 'jm_slowunet_v3.hdf5', monitor='loss', save_best_only=True)
-for i in range(NUM_EPOCHS):
-    random.shuffle(file_list)
-    print 'Epoch: %d/%d' % (i, NUM_EPOCHS)
-    for j in range(30):
-        X_train, Y_train = load_patients(file_list[j*20:(j+1)*20])
-        model.fit(X_train, Y_train, verbose=1, nb_epoch=1, batch_size=2, validation_data=(X_test, Y_test), shuffle=True, callbacks=[tb])
-    model.save(model_path + 'jm_slowunet_v4.hdf5')
-
-
-
-# print('Creating validation set...\n')
-# X_val = []
-# Y_val = []
-# i=0
-# for is_valid, (X, Y_mask, Y) in dataset.get_data('valid', 1, normalize):
-#     if is_valid:
-#         print("Iteration: %d" % i)
-#         i+=1
-#         X_val.append(X[0,0])
-#         Y_val.append(Y_mask[0,0])
-# X_val = np.expand_dims(np.asarray(X_val),axis=1)
-# Y_val = np.expand_dims(np.asarray(Y_val),axis=1)
-#
-#
-# print('Creating training set...\n')
-# X_tot = []
-# Y_tot = []
-# i=0
-# for is_valid, (X, Y_mask, Y) in dataset.get_data('train', 1, normalize):
-#     if len(X_tot)>10:
-#         break
-#     if is_valid:
-#         print("Iteration: %d" % i)
-#         i+=1
-#         X_tot.append(X[0,0])
-#         Y_tot.append(Y_mask[0,0])
-# X_tot = np.expand_dims(np.asarray(X_tot),axis=1)
-# Y_tot = np.expand_dims(np.asarray(Y_tot),axis=1)
+# print('Training...\n')
+# model_checkpoint = keras.callbacks.ModelCheckpoint(model_path + 'jm_slowunet_v3.hdf5', monitor='loss', save_best_only=True)
+# for i in range(NUM_EPOCHS):
+#     random.shuffle(file_list)
+#     print 'Epoch: %d/%d' % (i, NUM_EPOCHS)
+#     for j in range(30):
+#         X_train, Y_train = load_patients(file_list[j*20:(j+1)*20])
+#         model.fit(X_train, Y_train, verbose=1, nb_epoch=1, batch_size=2, validation_data=(X_test, Y_test), shuffle=True, callbacks=[tb])
+#     model.save(model_path + 'jm_slowunet_v4.hdf5')
 
 
 
+from skimage import measure
+def get_regions(nodule_mask):
+    thr = np.where(nodule_mask < np.mean(nodule_mask), 0., 1.0)  # threshold detected regions
+    label_image = measure.label(thr)  # label them
+    labels = label_image.astype(int)
+    regions = measure.regionprops(labels)
+    return regions
+
+def intersection_regions(r1, r2):
+    h = min(r1.bbox[2], r2.bbox[2]) - max(r1.bbox[0], r2.bbox[0])
+    w = min(r1.bbox[3], r2.bbox[3]) - max(r1.bbox[1], r2.bbox[1])
+    intersectionArea = w*h
+    if h<0 or w<0:
+        return 0.0
+
+    area1 = (r1.bbox[2] - r1.bbox[0])*(r1.bbox[3] - r1.bbox[1])
+    area2 = (r2.bbox[2] - r2.bbox[0])*(r2.bbox[3] - r2.bbox[1])
+    unionArea = area1 + area2 - intersectionArea
+    overlapArea = intersectionArea*1.0/unionArea # This should be greater than 0.5 to consider it as a valid detection.
+    return overlapArea
 
 
-# ## TRAIN
-# for i_epoch in range(num_epoch):
-#     print("Current epoch " + str(i_epoch))
-#
-#     ## TRAIN CHUNK BY CHUNK
-#
-#     for is_valid, (X, Y_mask, Y) in dataset.get_data('train', 50, normalize):
-#         if is_valid:
-#             model.fit(X, Y_mask, verbose=1, nb_epoch=1, batch_size=2, shuffle=True, callbacks=[tb])  # validation_split=.25
-#
-#         # if i%%10==0:
-#         #     print 'Predicting'
-#         #     Y_pred = model.predict(X)
-#         #     plt.imshow(X[0,0])
-#         #     plt.show()
-#         #     plt.imshow(Y_mask[0,0])
-#         #     plt.show()
-#         #     plotting.plot_mask(X[0,0], Y_mask[0,0])
-#         #     plt.imshow(Y_pred[0,0])
-#         #     plt.show()
-#         #     dice_coef_np(y_true=Y_mask[0,0].astype(np.float32), y_pred=Y_pred[0,0])
-#         #     #Y_mask.astype(np.float32)
-#         #     np.save()
-#
-#     ## SAVE MODEL AFTER 1 EPOCH
-#     model.save(model_path + 'jm_slowunet.hdf5')
-#
-#     ## VALIDATE THE MODEL ON VALID SET
-#     vals_metrics = []
-#     for is_valid, (X, Y_mask, Y) in dataset.get_data('valid', max_data_chunk, normalize):
-#         if is_valid:
-#             vals_metrics.append(model.evaluate(X,Y_mask,batch_size=max_batch_size))
-#     print("Validation loss " + str(np.mean(vals_metrics)))
+tp, fp, fn = 0, 0, 0
+for j in range(20):
+    X_test, Y_test = load_patients(file_list[j*10:(j+1)*10])
+
+    print 'Predicting... %d' % j
+    pred = model.predict([X_test], verbose=0)
+
+    # # plots
+    # idx = 1
+    # plt.imshow(pred[idx,0])
+    # plt.show()
+    # plot_mask(X_test[idx,0], pred[idx,0])
+    # plot_mask(X_test[idx,0], Y_test[idx,0])
+
+    print 'Evaluating... %d' % j
+    for i in range(pred.shape[0]):
+        regions_pred = get_regions(pred[i,0])
+        regions_real = get_regions(Y_test[i,0])
+        for region_real in regions_real:
+            detected = False
+            for region_pred in regions_pred:
+                # discard regions that occupy everything
+                if region_real.bbox[0]==0 or region_pred.bbox[0]==0:
+                    continue
+                score = intersection_regions(r1=region_pred, r2=region_real)
+                print 'i:%d, score:%s' % (i, str(score))
+                if score>.5:
+                    tp+=1
+                    detected = True
+                else:
+                    fp+=1
+            if not detected:
+                fn += 1
+
+    print 'tp:%d, fp:%d, fn:%d' % (tp,fp,fn)
 
 
 # VISUALIZE RESULTS
