1f800e7c91a89f2f3538028d1a590e66451605c7
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2017-03-05 23:17:05 +0100

    proves

--- src/jm_analysis/fp_reducer.R
+++ src/jm_analysis/fp_reducer.R
@@ -1,38 +1,50 @@
 library(data.table)
 
 
-data = data.table(read.csv('/Users/mingot/Projectes/kaggle/ds_bowl_lung/data/final_model/output_model_teixi_luna.csv'))
-data = data[x!=0 & x!=511 & y!=0 & y!=511] 
+luna_data = data.table(read.csv('/Users/mingot/Projectes/kaggle/ds_bowl_lung/data/final_model/output_model_teixi_luna.csv'))
+luna_data = luna_data[x!=0 & x!=511 & y!=0 & y!=511] 
 
-hog = data.table(read.csv('/Users/mingot/Projectes/kaggle/ds_bowl_lung/data/final_model/hog_v1.csv', header=F))
+#hog = data.table(read.csv('/Users/mingot/Projectes/kaggle/ds_bowl_lung/data/final_model/hog_v1.csv', header=F))
+hog = data.table(read.csv('/Users/mingot/Projectes/kaggle/ds_bowl_lung/data/final_model/hog_v2_total.csv', header=F))
 setnames(hog, c('V1','V2','V3','V4','V5'), c('score','filename','nslice','x','y'))
 
 
-data = merge(data, hog, by=c("filename","nslice","x","y"), all.x=T)
+luna_data = merge(luna_data, hog, by=c("filename","nslice","x","y"), all.x=T)
 
 # TEMPORAL!!
-data = data[!is.na(score)]
-data[,target:=as.numeric(score>0.3)]
+luna_data = luna_data[!is.na(score)]
+luna_data[,target:=as.numeric(score>0.1)]  # dangerous!
 
-mymodel = glm(target ~. - filename - score, family=binomial(link='logit'), data=data)
+## reduce 0's to make it balanced
+cands = which(luna_data$target==0)
+data0 = luna_data[sample(cands,0.1*length(cands))]
+luna_data = rbind(luna_data[target==1], data0)
+mean(luna_data$target)
 
-k = 4 #Folds
-xx$id = sample(1:k, nrow(xx), replace = TRUE)
+
+k = 3 #Folds
+luna_data$id = sample(1:k, nrow(luna_data), replace = TRUE)
 list = 1:k
+aucs = c()
 
 for (i in 1:k){
-  trainingset = subset(xx, id %in% list[-i])
-  testset = subset(xx, id %in% c(i))
+  trainingset = subset(luna_data, id %in% list[-i])
+  testset = subset(luna_data, id %in% c(i))
   
   # train
-  #mymodel = randomForest(trainingset$target ~ ., data = trainingset, ntree = 100)
-  mymodel = glm(target ~. - filename - score, family=binomial(link='logit'), data=xx)
-  
+  # fp_model = randomForest(trainingset$target ~ ., data = trainingset, ntree = 100)
+  # fp_model = glm(target ~. - filename - score, family=binomial(link='logit'), data=data)
+  fp_model = glm(target ~ 1 + nslice + x + y + diameter + max_intensity + min_intensity + mean_intensity,
+                family=binomial(link='logit'), data=trainingset)
+
   # test
-  pred = predict(mymodel, testset)
-  real = testset$cancer
+  pred = predict(fp_model, testset, type="response")
+  real = testset$target
   
   cat(auc(real,pred),"\n")
+  aucs = c(aucs, auc(real,pred))
 }
+mean(aucs)
+sd(aucs)
 
 
--- src/jm_analysis/patients.R
+++ src/jm_analysis/patients.R
@@ -1,29 +1,44 @@
 library(data.table)
 
-annotations = data.table(read.csv('/Users/mingot/Projectes/kaggle/ds_bowl_lung/data/stage1_labels.csv'))
-data = data.table(read.csv('/Users/mingot/Projectes/kaggle/ds_bowl_lung/data/final_model/dl_unet_v01.csv'))
+data = data.table(read.csv('/Users/mingot/Projectes/kaggle/ds_bowl_lung/data/final_model/dl_unet_v01_mingot_pc.csv'))
 data[,patientid:=gsub("dsb_","",patientid)]
-data = merge(data, annotations, by.x="patientid", by.y="id", all.x=T)
-data = data[!is.na(cancer)]  # remove test (for which we do not have cancer label)
+# hog = data.table(read.csv('/Users/mingot/Projectes/kaggle/ds_bowl_lung/data/final_model/hog_v1.csv', header=F))
+# setnames(hog, c('V1','V2','V3','V4','V5'), c('score','patientid','nslice','x','y'))
+# hog[,patientid:=gsub("luna_","",patientid)]
+# hog[,patientid:=gsub(".npz","",patientid)]
 
-luna_nodules = data.table(read.csv('/Users/mingot/Projectes/kaggle/ds_bowl_lung/data/luna/annotations.csv'))
-summary(luna_nodules$diameter_mm)
-quantile(luna_nodules$diameter_mm,0.9)
+# luna_nodules = data.table(read.csv('/Users/mingot/Projectes/kaggle/ds_bowl_lung/data/luna/annotations.csv'))
+# summary(luna_nodules$diameter_mm)
+# quantile(luna_nodules$diameter_mm,0.9)
 
 # FILTER 1: extreme windows (1.5M -> 1.1M)
 data = data[x!=0 & x!=511 & y!=0 & y!=511]  
 
-# FILTER 2: small and large nodules (1.1M -> 0.78M)
-# From cancer paper:
-# Benign nodule size (mm): mean: 4.1+-3.1, median: 3.4, quartiles: 2.7-5.0
-# Malign nodule size (mm): mean: 15.7+-12.2, median: 13, quartiles: 8-19
-# each pixel is 7mm. We accept diameter in [2.5mm, 20mm] -> [3.5, 28] pixel units of diameter
-summary(data$diameter)
-data = data[diameter %between% c(3.5, 28)]
+# # FILTER 2: small and large nodules (1.1M -> 0.78M)
+# # From cancer paper:
+# # Benign nodule size (mm): mean: 4.1+-3.1, median: 3.4, quartiles: 2.7-5.0
+# # Malign nodule size (mm): mean: 15.7+-12.2, median: 13, quartiles: 8-19
+# # each pixel is 7mm. We accept diameter in [2.5mm, 20mm] -> [3.5, 28] pixel units of diameter
+# summary(data$diameter)
+# data = data[diameter %between% c(3.5, 28)]
 
 
-# just high intensity nodules
-data = data[mean_intensity>0.8]
+# # FILTER 3: just high intensity nodules
+# data = data[mean_intensity>0.8]
+
+# FILTER 4: 
+data[,nodule_pred:=predict(fp_model, data, type="response")]
+data = data[nodule_pred>quantile(nodule_pred,0.9)]
+
+## Add annotations
+annotations = data.table(read.csv('/Users/mingot/Projectes/kaggle/ds_bowl_lung/data/stage1_labels.csv'))
+setnames(annotations, "id", "patientid")
+data = merge(annotations, data, by="patientid", all.x=T)
+data = data[!is.na(cancer)]  # remove test (for which we do not have cancer label)
+
+mean(data[!is.na(nslice)]$cancer)
+mean(data[is.na(nslice)]$cancer)
+
 
 # data exploration --------------------------------------------------------
 library(ggplot2)
@@ -56,7 +71,7 @@ arcvi_descriptivos = function(feature,target,equidistributed=T,bins=10){
   return(df)
 }
 
-# nodules distribution
+# visualize nodules distribution
 library(MASS)
 pid = sample(unique(data$patientid),1)
 dens = kde2d(data[patientid==pid]$x, data[patientid==pid]$y)
@@ -64,41 +79,48 @@ filled.contour(dens)
 xx[patientid==pid]
 
 # nodules per person
-xx = data[,.(total_nodules=.N, nodules_per_slice=.N/uniqueN(nslice),
-             num_slices=uniqueN(nslice), 
-             max_intensity=max(max_intensity), max_mean_intensity=max(mean_intensity)),
+final_df = data[,.(total_nodules=sum(!is.na(nslice)), 
+                   num_slices=uniqueN(nslice[!is.na(nslice)]), 
+                   nodules_per_slice=sum(!is.na(nslice))/uniqueN(nslice),
+                   max_intensity=max(max_intensity, na.rm=T), 
+                   max_mean_intensity=max(mean_intensity, na.rm=T)),
           by=.(patientid,cancer)]
 
-arcvi_descriptivos(xx$N, xx$cancer, equidistributed=F)
-arcvi_descriptivos(xx[max_mean_intensity>0.853]$max_mean_intensity, xx[max_mean_intensity>0.853]$cancer, equidistributed=F)
+final_df[max_intensity==-Inf, max_intensity:=0]
+final_df[max_mean_intensity==-Inf, max_mean_intensity:=0]
 
-xx[,high_intensity:=ifelse(max_mean_intensity>0.9,1,0)]
-xx[,lot_nodules:=ifelse(total_nodules>150,1,0)]
-xx[,few_nodules:=ifelse(total_nodules<50,1,0)]
+arcvi_descriptivos(final_df$N, final_df$cancer, equidistributed=F)
+arcvi_descriptivos(final_df[max_mean_intensity>0.853]$max_mean_intensity, 
+                   final_df[max_mean_intensity>0.853]$cancer, equidistributed=F)
 
-auc(xx$cancer, xx$lot_nodules)  # 0.51
-auc(xx$cancer, xx$few_nodules)  # 0.49
-auc(xx$cancer, xx$high_intensity)  # 0.49
+final_df[,high_intensity:=ifelse(max_mean_intensity>0.9,1,0)]
+final_df[,lot_nodules:=ifelse(total_nodules>150,1,0)]
+final_df[,few_nodules:=ifelse(total_nodules<50,1,0)]
 
-model = glm(cancer ~. - patientid, family=binomial(link='logit'), data=xx)
+auc(final_df$cancer, final_df$lot_nodules)  # 0.51
+auc(final_df$cancer, final_df$few_nodules)  # 0.49
+auc(final_df$cancer, final_df$high_intensity)  # 0.49
+
+model = glm(cancer ~. - patientid, family=binomial(link='logit'), data=final_df)
 summary(model)
 
-
+final_df[,patientid:=as.character(patientid)]
 
 k = 4 #Folds
-xx$id = sample(1:k, nrow(xx), replace = TRUE)
+final_df$id = sample(1:k, nrow(final_df), replace = TRUE)
 list = 1:k
 
 for (i in 1:k){
-  trainingset = subset(xx, id %in% list[-i])
-  testset = subset(xx, id %in% c(i))
+  trainingset = subset(final_df, id %in% list[-i])
+  testset = subset(final_df, id %in% c(i))
   
   # train
   #mymodel = randomForest(trainingset$target ~ ., data = trainingset, ntree = 100)
-  mymodel = glm(cancer ~. - patientid, family=binomial(link='logit'), data=xx)
+  mymodel = glm(cancer ~ . -id, family=binomial(link='logit'), 
+                data=trainingset[,!names(testset)%in%'patientid',with=F])
   
   # test
-  pred = predict(mymodel, testset)
+  pred = predict(mymodel, testset[,!names(testset)%in%'patientid',with=F])
   real = testset$cancer
 
 cat(auc(real,pred),"\n")
