0a5cebf63f54bc24b566b303a0c4ee032758100c
felix <felix.axel.gimeno@estudiant.upc.edu>
2017-03-08 19:22:44 +0000

    fix

--- src/jc_dl/experiments/slice_classification/_2_train_network.py
+++ src/jc_dl/experiments/slice_classification/_2_train_network.py
@@ -14,9 +14,9 @@ import math
 from time import time
 from keras.optimizers import Adam
 from keras import backend as K
-sys.path.append(os.path.dirname(__file__) + "../../")
-from networks.sample_cnn import Sample2DCNNNetworkArchitecture
-from networks.sample_resnet import ResnetBuilder
+sys.path.append(os.path.dirname(__file__) + "../../../")
+#from networks.sample_cnn import Sample2DCNNNetworkArchitecture
+from dl_networks.sample_resnet import ResnetBuilder
 from utils.tb_callback import TensorBoard
 from keras.callbacks import LearningRateScheduler
 from keras.layers.normalization import BatchNormalization
@@ -30,8 +30,8 @@ USE_EXISTING = False  # load previous model to continue training
 
 
 ## paths
-model_path  = '/mnt/hd2/models'
-custom_dataset_path = '/mnt/hd2/custom_dataset_jc'
+model_path  = '/mnt/hd2/models/'
+custom_dataset_path = '/mnt/hd2/custom_dataset_jc/'
 logs_path = '/mnt/hd2/logs/slice_%s' % str(int(time()))
 
 def get_data_from_file(filename):
@@ -53,7 +53,7 @@ tb = TensorBoard(log_dir=logs_path, histogram_freq=1, write_graph=False, write_i
 
 
 print 'creating model...'
-model = ResnetBuilder().build_resnet_18((1,512,512),1)
+model = ResnetBuilder().build_resnet_18((512,1,512),1)
 model.compile(optimizer=Adam(lr=.5e-2), loss='binary_crossentropy', metrics=['accuracy','fmeasure'])
 #model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])  # metric which will be used is defined here
 
@@ -80,9 +80,10 @@ for i in range(NUM_EPOCHS):
 for i in range(NUM_EPOCHS):
     random.shuffle(train_files)
     for file in train_files:
-        X_train, Y_train = get_data_from_file(file)
+	X_train, Y_train = get_data_from_file(file)
         X_train = normalize(X_train, X_test.mean(), X_test.std())
-        print("Ys labeled as 1s: " + str(Y_train.sum()))
+	print "X_train shape" + str(X_train.shape)
+        print "Ys labeled as 1s: " + str(Y_train.sum())
         model.fit(X_train, Y_train, class_weight = {0:1.,1:10.}, verbose=1, nb_epoch=1, batch_size=BATCH_SIZE, validation_data=(X_test, Y_test), shuffle=True, callbacks=[tb])
         model.save(model_path + 'fg_sampleresnet18_v0.hdf5')
         del X_train
