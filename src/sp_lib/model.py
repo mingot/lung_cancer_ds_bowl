03021466237802d193e7c15365179f7e9c214594
SergiPicart <elpicart@gmail.com>
2017-03-11 12:44:56 +0100

    First approximation: features from 2D patches. Missing HOG and LBP

---
+++ src/sp_lib/model.py
@@ -0,0 +1,244 @@
+import numpy as np
+from math import ceil
+import pandas as pd
+import scipy.misc as spm
+def extract_patch(
+    df, 
+    patient_id, 
+    patient_path, 
+    out_size=(25, 25), 
+    swap_xy=False, 
+    C=-500, 
+    W=1500, 
+    verbose=False):
+    """
+    This function extracts, from the deep learning data_frame and 
+    a patient id, the following as an ordered dict: 
+    (1) original patch, square 
+    (2) original lung mask
+    (3) resized patch
+    (4) resized lung mask
+    
+    df: data_frame from deep learning
+    patient_id: patient_code.npz to compute
+    patient_path: path where the .npz files are stored
+    out_size: size of the resized patches
+    swap_xy: should x and y be swapped? Debug purposes
+    C: center of filtering
+    W: width of filtering
+    verbose: show debug messages
+    """
+    # data frame for the patient and preprocessd file
+    np_pat = np.load(patient_path + '/' + patient_id)['arr_0']
+    df_pat = df[df['patientid'] == patient_id]
+    
+    patches = []
+    for ind, row in df_pat.iterrows(): 
+        z = int(row['nslice'])
+        cx = int(row['centroidx'])
+        cy = int(row['centroidy'])
+        r = int(ceil(row['diameter']/2.))
+        
+        if verbose: 
+            print 'Slice: {} CX: {} CY: {}'.format(z, cx, cy)
+        
+        x = range(cx - r, cx + r + 1)
+        y = range(cy - r, cy + r + 1)
+    
+        hu = np_pat[0, z]
+        hu[hu < C - W/2] = C - W/2
+        hu[hu > C + W/2] = C + W/2
+        lung = np_pat[1, z]
+        
+        if swap_xy:
+            img_hu = hu[np.ix_(y, x)]
+            img_lung = lung[np.ix_(y, x)]
+        else:
+            img_hu = hu[np.ix_(x, y)]
+            img_lung = lung[np.ix_(x, y)]
+        
+        
+        resc_hu = spm.imresize(img_hu, out_size)
+        resc_lung = spm.imresize(img_lung, out_size, interp='lanczos')
+        resc_lung = np.where(resc_lung > 255/2., 1.0, 0.0)
+        
+        # if all the pixels are lung, mask should be all ones
+        # (this fails by default)
+        if img_lung.astype(int).sum() == np.array(img_lung.shape).prod(): 
+            resc_lung[:, :] = 1
+        
+        patches.append({'img_hu': img_hu, 
+                        'img_lung': img_lung, 
+                        'resc_hu': resc_hu, 
+                        'resc_lung': resc_lung
+        })
+    return patches, df_pat
+
+
+import collections
+import skimage.segmentation as sks
+import skimage.filters as skfi
+# from skimage.draw import circle_perimeter
+from scipy import ndimage as ndi
+import skimage.morphology as skm
+import skimage.measure as skme
+from skimage.morphology import disk, square
+def process_img(img, lung_mask, return_images=False): 
+    """
+    This function processes the given patch and returns the 
+    skimage metrics properties object 
+    (by now, largest segmented area)
+    
+    img: grayscale patch
+    lung_mask: binary mask for lung pixels
+    return_images: return images instead of regionprops (useful for plotting and debugging)
+    """
+    # 1 filtering
+    #ex_1 = skfi.sobel(img)
+    ex_1 = skfi.gaussian(img, sigma=1)
+    # plt.imshow(ex_1)
+    
+    
+    # 2 detect edges
+    #ex_2 = sks.active_contour(ex_1, )
+    #ex_2 = skf.canny(ex_1)
+    thresh = skfi.threshold_otsu(ex_1)
+    ex_2 = skm.closing(ex_1 > thresh, square(3))
+    
+    # plt.imshow(ex_2)
+    
+    # s = np.linspace(0, 2*np.pi,25)
+    # init = 10*np.array([np.cos(s), np.sin(s)]).T+12.5
+    # snake = sks.active_contour(ex, init, alpha=1e-3, beta=0.4)
+    
+    # plot the snake
+    # axes = plt.gca()
+    # axes.set_xlim([0, 24])
+    # axes.set_ylim([0, 24])
+    # plt.plot(snake[:, 0], snake[:, 1], linewidth = 3.0, color = "r")
+    # plt.imshow(ex)
+    
+    # apply lung mask
+    ex_2_lung = ex_2*lung_mask
+    
+    # 3 fill regions
+    ex_3 = ndi.binary_fill_holes(ex_2_lung)
+    
+    # plt.imshow(ex_3)
+    
+    # 4 opening
+    ex_4 = skm.opening(ex_3, skm.square(3))
+    # plt.imshow(ex_4)
+    
+    # 5 remove border
+    #  this can clear everything if the node is ON the border
+    ex_5 = sks.clear_border(ex_4)
+    if ex_5.sum() == 0:
+        ex_5 = ex_4
+    # plt.imshow(ex_5)
+    
+    # 6 labelling
+    ex_6 = skme.label(ex_5)
+    
+    # 7 properties
+    # props = skme.regionprops(ex_6, ex_1)
+    # len(props)
+    # props = props[0]
+    # for prop in props:
+    #     print(prop, props[prop])
+    # props.area
+    # props.inertia_tensor_eigvals
+    
+    # ex_6.sum()
+    
+    # props[0].area
+    
+    # 8 raw features
+    # feat_lbp = skf.local_binary_pattern(ex_1, P=8, R=3, method='uniform')
+    # feat_lbp.shape
+    # plt.imshow(feat_lbp)
+    # plt.hist(feat_lbp.ravel())
+    ans = collections.OrderedDict()
+    ans['0_raw'] = img
+    ans['1_filter'] = ex_1
+    ans['2_otsu'] = ex_2
+    ans['2b_lungmask'] = lung_mask
+    ans['3_fillholes'] = ex_3
+    ans['4_opening'] = ex_4
+    ans['5_clearborder'] = ex_5
+    ans['6_labelling'] = ex_6
+    
+    if return_images: 
+        return ans
+        
+    # features
+    ans_prop = skme.regionprops(ex_6, ex_1)
+    
+    # no region found?
+    if len(ans_prop) == 0:
+        return None
+    
+    # if labelling found several regions, we only take the largest for now
+    ans_size = [p.area for p in ans_prop]
+    ans_whichmax = ans_size.index(max(ans_size))
+    ans_max = ans_prop[ans_whichmax]
+    
+    return ans_max
+    # return ans, ans_prop
+
+
+
+
+import collections
+import skimage.segmentation as sks
+import skimage.filters as skfi
+# from skimage.draw import circle_perimeter
+from scipy import ndimage as ndi
+import skimage.morphology as skm
+import skimage.measure as skme
+def process_prop(prop):
+    """
+    This function extracts some features from a scikit image
+    properties object, as an ordered dict.
+    
+    prop: object from regionprops (skimage.measure._regionprops._RegionProperties)
+    """
+    if prop is None:
+        return None
+        
+    return pd.DataFrame.from_records([{
+        '01_eccentricity': prop.eccentricity, 
+        '02_extent': prop.extent, 
+        '03_area': prop.area, 
+        '04_perimeter': prop.perimeter, 
+        '05_solidity': prop.solidity, 
+        '06_mean_intensity': prop.mean_intensity, 
+        '07_max_intensity': prop.max_intensity, 
+        '08_inertia_tensor_eigvals0': prop.inertia_tensor_eigvals[0], 
+        '09_inertia_tensor_eigvals1': prop.inertia_tensor_eigvals[1]
+    }])
+
+import matplotlib.pyplot as plt
+def process_plot(list_dict):
+    """
+    Plot images from process_img in a grid
+    
+    list_dict: patches from process_img. A list whose elements are the dictionaries
+        generated with process_img
+    """
+    
+    n_row = len(list_dict)
+    n_col = len(list_dict[0])
+    
+    fig, axs = plt.subplots(
+        nrows = n_row, ncols = n_col, figsize = (25, 25))
+    plt.gray()
+    # fig.subplots_adjust(hspace = .5, wspace=.001)
+    #axs = axs.ravel()
+    
+    for ax_row, dict_plot in enumerate(list_dict): 
+        for ax_col, (key, value) in enumerate(dict_plot.iteritems(), 0):
+            axs[ax_row, ax_col].imshow(value)
+            axs[ax_row, ax_col].set_title(key)
+    
+    plt.show()
---
+++ src/sp_lib/sp_feat_dl1.py
@@ -0,0 +1,113 @@
+import os
+import pandas as pd
+import numpy as np
+import re
+
+from math import sqrt,ceil
+from skimage.feature import blob_dog, blob_log, blob_doh
+from skimage.color import rgb2gray
+import skimage.feature as skf
+import skimage.filters as skfi
+
+from skimage.filters.rank import entropy
+from skimage.morphology import disk, square
+
+import matplotlib.pyplot as plt
+import pickle as pk
+import scipy.misc as spm
+
+# PATH_DSB = "/media/sergi/Seagate Expansion Drive/preprocessed5/"
+PATH_DSB = "/home/sergi/all/devel/big/lung_cancer_ds_bowl/preprocessed5/"
+PATH_SRC = os.environ.get('LUNG_PATH') + "src"
+os.chdir(PATH_SRC)
+PATH_OUT = os.environ.get('LUNG_PATH') + "data/sp_data/dl/"
+PATH_DL = os.environ.get('LUNG_PATH') + "/data/output_dl.csv"
+
+# load and filter DL results
+# removed last row (was half written)
+df_dl = pd.read_csv(PATH_DL)
+df_dl_filter = df_dl[df_dl['centroidx'].between(1, 510) & 
+    df_dl['centroidy'].between(1, 510) & 
+    df_dl['diameter'].between(3.5, 28)]
+
+# explore data
+df_grouped = df_dl_filter.groupby("patientid")
+df_grouped.sum()
+
+
+# k = np.load(PATH_DSB + 'dsb_00edff4f51a893d80dae2d42a7f45ad1.npz')['arr_0']
+# k.shape
+
+# a=df_dl.iloc[[1]]
+# a['diameter']
+
+# my crappy functions
+import sp_lib.model as splm
+
+# Example code for a given patient 
+test_pat = 'dsb_00edff4f51a893d80dae2d42a7f45ad1.npz'
+
+# (1) Extract patchs from data frame and one patient
+p_patch, p_df = splm.extract_patch(
+    df_dl_filter, 
+    patient_id=test_pat, 
+    patient_path=PATH_DSB, 
+    swap_xy=False)
+# Images for this patient: original patch, rescaled patch, 
+#   original lung mask, rescaled lung mask
+# We will use the rescaled patches (hu units)
+# not filtered by HU units at the moment
+
+# (2) Extract properties (not features yet)
+p_prop = [splm.process_img(img['resc_hu'], img['resc_lung']) for img in p_patch]
+# Extract meaningful features
+# TODO: also use (weighted?) hu moments, HOG, LBP, use lung mask in the process
+# this returns 1-row dfs for each patch, or None 
+
+# (3) extract properties
+p_feat = [splm.process_prop(p) for p in p_prop]
+
+# p_filtered = [x is None for x in p_feat]
+# p_all = zip(p_df, p_feat)[p_filtered]
+# df_all = pd.concat([p_df.iloc[[ind]].astype(dict).append(feat) for ind, feat in enumerate(p_feat) if feat is not None])
+
+# Combine all data frames
+# removed slices 
+
+# (4) indices of the non-null patches (some patches are null because 
+# segmentation in (2) did not find anything)
+patch_nonnull = [x is not None for x in p_feat]
+
+# data frame with features
+# (5) data_frame of features
+df_feat = pd.concat(p_feat)
+# recover indices
+df_feat.index = p_df.index[patch_nonnull]
+
+# (6) concat data frames to obtain the final augmented data frame for this patient
+# df_all = pd.merge(p_df.iloc[patch_nonnull], df_feat, how='cross')
+df_all = pd.concat([p_df.iloc[patch_nonnull], df_feat], axis=1)
+# (p_df.iloc[patch_nonnull]).shape
+# df_feat['patientid'] = p_df.patientid[[x is not None for x in p_feat]].tolist()
+# df_all = p_df.merge(df_feat, on = 'patientid')
+
+# (7) see it worked
+df_all.head()
+
+# plot intermediate images
+# p_img = [splm.process_img(img['resc_hu'], img['resc_lung'], return_images=True) for img in p_patch]
+# splm.process_plot(p_img[00:10])
+
+
+# visualise the patient and reported patches
+# len(p_patch), len(p_prop), len(p_feat)
+
+# x_pat = np.load(PATH_DSB + test_pat)['arr_0']
+
+# df_dl_filter.iloc[0:10]
+# df_dl_filter[df_dl_filter['patientid'] == test_pat].iloc[0:10]
+# plt.imshow(x_pat[1, 53])
+# from utils.plotting import cube_show_slider
+
+# cube_show_slider(x_pat[0]*x_pat[1])
+# plt.imshow(x_pat[0, 47])
