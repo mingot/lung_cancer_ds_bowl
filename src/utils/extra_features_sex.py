8422ac1f593d0d76a927c126d04d373a13d6c7da
Carlos <carlos.gonzalez.rotger@est.fib.upc.edu>
2017-03-16 13:00:54 +0100

    new segmentation for chest, isolating mammal glands

--- src/utils/extra_features_sex.py
+++ src/utils/extra_features_sex.py
@@ -6,14 +6,15 @@ from reading import *
 from plotting import *
 from preprocessing import *
 from scipy.ndimage.morphology import *
-from skimage.measure import moments, moments_central, moments_normalized, moments_hu
+from skimage import filters
+from skimage.measure import label, regionprops, moments, moments_central, moments_normalized, moments_hu
 from scipy.signal import periodogram, find_peaks_cwt
 from scipy.stats import describe
 from multiprocessing import Pool
 
 import matplotlib.pyplot as plt
 
-DEBUG = False
+DEBUG = True
 SERVER = os.uname()[1] == 'ip-172-31-7-211'
 
 if SERVER:
@@ -23,14 +24,14 @@ if SERVER:
 else:
     path = '/home/carlos/DSB2017/dsb_sample'
     preprocessed = '/home/carlos/DSB2017/dsb_preprocessed'
-    output_file = '/home/carlos/lung_cancer_ds_bowl/data/stage1_extra_features_sex_predictors_prueba.csv'
+    output_file = '/home/carlos/lung_cancer_ds_bowl/data/stage1_extra_features_sex_predictors_simple.csv'
 
 patient_files = os.listdir(path)
 patient_files = sorted(patient_files)
 
-#patient_files = ['91d0606b85ab7dbc7bab718c1312f2df']
+patient_files = ['4d86e1657d46b9ee44c2c434fad231ce', '579dfed23d49414a2c080bbf9d7f958f', '5fd33ea74e1ad740a201ae9b3c383fc5', '8269e2eb69b3ba38c48dd5ae6039e250']
 
-common_spacing = [2., 0.6, 0.6]
+common_spacing = [2., 0.7, 0.7]
 
 def __3d_sobel__(pix):
     edges = np.array([ scipy.ndimage.filters.sobel(z_pix) for z_pix in pix ])
@@ -77,9 +78,7 @@ def __get_lung_height__(lung_mask):
         return 0
     else: 
         return np.max(exists_mask) - np.min(exists_mask)
-  
-def __delete_base__(pix):
-    return  
+
 
 def get_intercostal_dist(pix, bone_mask, new_spacing, lung_height):
     '''
@@ -141,34 +140,106 @@ def __to_positive__(pix):
     #return 255.*1000./3500. + 255./3500. * pix
     return 1100 + pix
 
-def __segment_interior__(pix, lung_mask):
+def __segment_chest__(pix, lung_mask):
+    erode = 10
     mask = lung_mask.astype(bool)
-#    cube_show_slider(np.where(mask, 10000, pix))
-    '''
-    for z in xrange(pix.shape[0]):
-        touch = False
-        k = 0
-        while not touch:
-            mask_ext[z,:,:] = scipy.ndimage.morphology.binary_dilation(mask[z,:,:], iterations=2)
-            diff = mask_ext[z,:,:] * (1 - mask[z,:,:])
-            bone_diff = diff*pix[z,:,:] > 350    
-            mask[z,:,:] = np.logical_or(mask[z,:,:], diff*(1 - bone_diff))
-            k += 1
-            touch = np.any(bone_diff) or k == 20
-        
-    cube_show_slider(np.where(mask, pix, -3400))
-    cube_show_slider(np.where(mask, 10000, pix))
-    '''
     mask = scipy.ndimage.morphology.binary_dilation(mask, iterations=20)
-    mask = scipy.ndimage.morphology.binary_closing(np.logical_or(mask, pix <= -1000), iterations=10)
+    mask = scipy.ndimage.morphology.binary_closing(np.logical_or(mask, pix <= -1000), iterations=erode)
     zs = np.array(range(pix.shape[0])) 
     zs = np.logical_or( zs < pix.shape[0]/5, zs > 4*pix.shape[0]/5 )
+    xs = np.array(range(pix.shape[2]))
+    xs = np.logical_or(xs < erode, xs > pix.shape[2]-erode)
+    ys = np.array(range(pix.shape[1]))
+    ys = ys < pix.shape[1] / 2
+    mask_xy = np.einsum('j,k->jk', ys, np.ones(pix.shape[2]))
     mask_z = np.einsum('i,jk->ijk', zs, np.ones(shape=(pix.shape[1], pix.shape[2])))
+    mask_x = np.einsum('ij,k->ijk', np.ones(shape=(pix.shape[0], pix.shape[1])), xs)
+    mask_y = np.einsum('ik,j->ijk', np.ones(shape=(pix.shape[0], pix.shape[2])), np.array(range(pix.shape[1])) < erode)
+    mask = np.einsum('ijk,jk->ijk', mask, mask_xy)
     mask = np.logical_or(mask, mask_z)
-    return mask
+    mask = np.logical_or(mask, mask_x)
+    mask = np.logical_or(mask, mask_y)
+    mask = np.logical_or(mask, np.einsum('i,jk->ijk',1-zs,1-mask_xy))
+    labels = np.array([label(img_z) for img_z in 1-mask])
+    for img_label in labels:
+        areas = [r.area for r in regionprops(img_label)]
+        areas.sort()
+        if len(areas) >= 1:
+            for region in regionprops(img_label):
+                if region.area < areas[-1]:
+                    for coordinates in region.coords:
+                        img_label[coordinates[0], coordinates[1]] = 0
+    return labels > 0
     
 def __get_sex_predictors__(pix, mask):
+    masked = np.ma.array(pix, mask=(1-mask).astype(bool), fill_value=0)
+    mu = np.ma.mean(masked)
+    sd = np.ma.std(masked)
+    img = (masked - mu) / sd
+    imgarr = np.ma.filled(img)
+    #edges = np.array([ filters.frangi(imgarr[z,:,:], beta1=0.01, beta2=1.0) for z in xrange(imgarr.shape[0]) ])
+    edges_re, edges_im = map(np.array, zip(*[ filters.gabor(imgarr[z,:,:], frequency=10) for z in xrange(imgarr.shape[0]) ]))
+    binary = (edges_re > 10).astype(int)
+    
+    i = 0
+    cube_show_slider(img)
+    while i < 2:
+        binary = scipy.ndimage.morphology.binary_opening(binary, iterations=1).astype(int)
+        
+        
+        labels = label(binary, neighbors=8)
+        for img_label in labels:
+            areas = [r.area for r in regionprops(img_label)]
+            areas.sort()
+            if len(areas) >= 1:
+                for region in regionprops(img_label):
+                    if region.area == areas[-1] or region.area > 0.25*areas[-1] or region.area < 25:
+                        for coordinates in region.coords:
+                            img_label[coordinates[0], coordinates[1]] = 0
+        binary = labels > 0
+        binary = np.array([scipy.ndimage.morphology.binary_closing(bin, iterations=2).astype(int) for bin in binary])
+        print np.bincount(labels.flatten())
+        print np.sum(labels > 0)
+        i += 1
+    
+
+        cube_show_slider(binary, vmin=0, vmax=1)
+        cube_show_slider(labels, vmin=0, vmax=1)
+#    cube_show_slider(edges_re, vmin=np.min(edges_re), vmax=np.max(edges_re))
+#    cube_show_slider(edges_im, vmin=np.min(edges_im), vmax=np.max(edges_im))
+    return {'volume_pix': np.sum(labels > 0)}
+    '''
+    moms = [moments(img_z) for img_z in img]
+    moms_central = [moments_central(img[z,:,:], moms[z][0, 1] / moms[z][0, 0], moms[z][1, 0] / moms[z][0, 0]) for z in xrange(img.shape[0])]
+    moms_central_norm = [ moments_normalized(mom_central) for mom_central in moms_central ]
+    moms_hu = np.array([moments_hu(mom_central_norm) for mom_central_norm in moms_central_norm])
+    means = np.mean(moms_hu, axis=0)
+    
+    features = dict({'below0':np.sum(img < 0), 'totalmask': np.sum(mask), 'fraction': np.sum(img<0) * 1. / np.sum(mask)})
+    return dict( dict(zip(['mean_moments_hu_'+str(i) for i in xrange(7)], means)), **features)
+    '''
+    '''
+    imgprops = [regionprops(bin, cache=True) for bin in binary if np.any(bin > 0)]
+    features = dict({'area': np.mean(np.array([prop[0].area for prop in imgprops])), \
+                     'eccentricity': np.mean(np.array([prop[0].eccentricity for prop in imgprops])), \
+                     'extent': np.mean(np.array([prop[0].extent for prop in imgprops])), \
+                     'perimeter': np.mean(np.array([prop[0].perimeter for prop in imgprops])), \
+                     'solidity': np.mean(np.array([prop[0].solidity for prop in imgprops])), \
+                     })
+    momentsdict = dict(zip([ 'mean_moments_hu_'+str(i) for i in xrange(7) ], np.mean(np.array([prop[0].moments_hu for prop in imgprops]), axis=0)))  
+    '''
+    
+    #print dict(features, **momentsdict)
+    #return dict(features, **momentsdict)
+    
+'''    
+def __get_sex_predictors__(pix, mask):
     img = np.array([img_z for img_z in __to_positive__(pix)*(1-mask) if np.any(img_z)]).astype(float)
+    #cube_show_slider(__3d_sobel__(pix)*(1-mask) > 300, vmin=0, vmax=1)
+    previous_thing = np.abs(__3d_sobel__((1-mask)*pix))
+    thing = scipy.ndimage.morphology.binary_dilation(np.logical_and(previous_thing < 600, previous_thing > 200), iterations=1)
+    #cube_show_slider(1-scipy.ndimage.morphology.binary_dilation(mask, iterations=1).astype(float), vmin=0.0, vmax=1.0)
+    cube_show_slider(previous_thing.astype(float), vmin=0, vmax=1)
     #cube_show_slider(img)
     moms = [moments(img_z, order=6) for img_z in img]
     moms_central = [moments_central(img[z,:,:], moms[z][0, 1] / moms[z][0, 0], moms[z][1, 0] / moms[z][0, 0], order=6) for z in xrange(img.shape[0])]
@@ -182,9 +253,10 @@ def __get_sex_predictors__(pix, mask):
     nobs, minmax, mean, variance, skewness, kurtosis = describe(moms_hu, axis=0)
     return dict(zip([ descr + '_moments_hu_'+str(i) for descr in ['mean','variance','skewness','kurtosis'] for i in xrange(7) ], np.concatenate((mean,variance,skewness,kurtosis))))
     #return dict(zip(['moment_'+str(i) for i in xrange(7)], moms_hu))
+'''
         
 def process_patient_file(patient_file):
-#    print patient_file.split('/')[-1]
+    print patient_file.split('/')[-1]
     patient = load_scan(os.path.join(path, patient_file))
     pix = get_pixels_hu(patient)  # From pixels to HU
 #    pix = np.flip(pix, axis=0)
@@ -218,9 +290,10 @@ def process_patient_file(patient_file):
     features = dict({ 'bone_density': bone_mass * 1. / bone_vol }, **features) 
     '''
     
-    interior_mask = __segment_interior__(pix_resampled, lung_mask)
+    chest_mask = __segment_chest__(pix_resampled, lung_mask)
     
-    sex_predictors = __get_sex_predictors__(pix_resampled, interior_mask)
+    #cube_show_slider(chest_mask, vmin=0, vmax=1)
+    sex_predictors = __get_sex_predictors__(pix_resampled, chest_mask)
     #print sex_predictors
     features = dict(sex_predictors, **features) 
     return features
