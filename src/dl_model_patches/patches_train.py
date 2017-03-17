fixed auc computation

src/dl_model_patches/patches_train.py
@@ -406,38 +406,38 @@ with open(OUTPUT_CSV, write_method) as file:
 
 
 
-## Checking
-filename = 'luna_129007566048223160327836686225.npz'
-b = np.load(os.path.join(INPUT_PATH, filename))['arr_0']
-for j in range(b.shape[1]):
-    if np.sum(b[2,j])!=0:
-        print j
-
-nslice = 95
-plotting.plot_mask(b[0,nslice], b[2,nslice])
-
-X, y, rois = load_patient(filename, discard_empty_nodules=False, output_rois=True, thickness=THICKNESS)
-print 'hola'
-
-for nslice in range(b.shape[1]):
-    if np.sum(b[2,nslice])!=0:
-        regions_pred = [r[1] for r in rois if r[0]==nslice]
-        regions_real = get_regions(b[2,nslice])
-        labels, stats = get_labels_from_regions(regions_real, regions_pred)
-        print nslice, stats
-
-sel_regions = []
-for r in rois:
-    sel_nslice, region = r
-    if sel_nslice==nslice:
-        sel_regions.append(region)
-
-plotting.plot_bb(b[0,nslice], sel_regions)
-
-
-### quality checks for ROIs detection
-for filename in file_list:
-    X, y = load_patient(filename, discard_empty_nodules=True, output_rois=False, thickness=0)
+# ## Checking
+# filename = 'luna_129007566048223160327836686225.npz'
+# b = np.load(os.path.join(INPUT_PATH, filename))['arr_0']
+# for j in range(b.shape[1]):
+#     if np.sum(b[2,j])!=0:
+#         print j
+#
+# nslice = 95
+# plotting.plot_mask(b[0,nslice], b[2,nslice])
+#
+# X, y, rois = load_patient(filename, discard_empty_nodules=False, output_rois=True, thickness=THICKNESS)
+# print 'hola'
+#
+# for nslice in range(b.shape[1]):
+#     if np.sum(b[2,nslice])!=0:
+#         regions_pred = [r[1] for r in rois if r[0]==nslice]
+#         regions_real = get_regions(b[2,nslice])
+#         labels, stats = get_labels_from_regions(regions_real, regions_pred)
+#         print nslice, stats
+#
+# sel_regions = []
+# for r in rois:
+#     sel_nslice, region = r
+#     if sel_nslice==nslice:
+#         sel_regions.append(region)
+#
+# plotting.plot_bb(b[0,nslice], sel_regions)
+#
+#
+# ### quality checks for ROIs detection
+# for filename in file_list:
+#     X, y = load_patient(filename, discard_empty_nodules=True, output_rois=False, thickness=0)
 
 
 