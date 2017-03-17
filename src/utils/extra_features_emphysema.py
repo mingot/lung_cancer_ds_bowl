0e6779890724e5e28eb05a1e0453e51cb6a9a809
Ricard Delgado <ricard.delgado@csem.ch>
2017-03-17 15:17:54 +0000

    Added fucntion to extract important features to determine emphysema

---
+++ src/utils/extra_features_emphysema.py
@@ -0,0 +1,21 @@
+from preprocessing import *
+from scipy import stats
+import matplotlib.pyplot as plt
+
+threshold = -250
+
+
+def get_emphysema_predictors(img, mask):
+
+    pix_1d = np.ndarray.flatten(img)
+    mask_1d = np.ndarray.flatten(mask)
+    pix_lung = pix_1d[mask_1d == 1]
+    gated_pix_lung = pix_lung[pix_lung < threshold]
+
+    # the histogram of the data
+    plt.hist(pix_lung, 50, normed=1, facecolor='green', alpha=0.75)
+
+    gated_skewness = stats.skew(gated_pix_lung)
+    gated_kurtosis = stats.kurtosis(gated_pix_lung)
+
+    return gated_skewness, gated_kurtosis
