3353c3dbdfb5e75e4f7218e0c620b492780e331e
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2017-03-25 12:52:13 +0100

    parallel

--- src/dl_model_patches/common.py
+++ src/dl_model_patches/common.py
@@ -259,17 +259,28 @@ def load_patient(patient_data, patient_nodules_df=None, discard_empty_nodules=Fa
 
 
 
-def multiproc_crop_generator(filenames, out_x_filename, out_y_filename, load_patient_func):
+def multiproc_crop_generator(filenames, out_x_filename, out_y_filename, load_patient_func, parallel=False):
     """loads patches in parallel and stores the results."""
-    pool = multiprocessing.Pool(4)
-    tstart = time()
-    x, y, stats = zip(*pool.map(load_patient_func, filenames))
 
     xf, yf, total_stats = [], [], {}
-    for i in range(len(x)):
-        xf.extend(x[i])
-        yf.extend(y[i])
-        total_stats = add_stats(total_stats, stats[i])
+
+    if parallel:
+        pool = multiprocessing.Pool(4)
+        tstart = time()
+        x, y, stats = zip(*pool.map(load_patient_func, filenames))
+
+        for i in range(len(x)):
+            xf.extend(x[i])
+            yf.extend(y[i])
+            total_stats = add_stats(total_stats, stats[i])
+    else:
+        for idx,filename in enumerate(filenames):
+            logging.info("Loading %d/%d" % (idx,len(filenames)))
+            x,y,stats = load_patient_func(filename)
+            xf.extend(x)
+            yf.extend(y)
+            total_stats = add_stats(total_stats, stats)
+
 
     logging.info('Total time: %.2f, total patients:%d, stats: %s' % (time() - tstart, len(x), total_stats))
     np.savez_compressed(out_x_filename, np.asarray(xf))
