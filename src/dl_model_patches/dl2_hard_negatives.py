d521c03499b08c05d9c61f537bdf5b7d9fe99129
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2017-03-26 11:55:22 +0200

    fix

--- src/dl_model_patches/dl2_hard_negatives.py
+++ src/dl_model_patches/dl2_hard_negatives.py
@@ -80,65 +80,65 @@ common.multiproc_crop_generator(filenames_test,
 ### TRAINING -------------------------------------------------------------------------------------------------------
 
 
-# Data augmentation generator
-train_datagen = ImageDataGenerator(dim_ordering="th", horizontal_flip=True, vertical_flip=True)
-test_datagen = ImageDataGenerator(dim_ordering="th")  # dummy for testing to have the same structure
+# # Data augmentation generator
+# train_datagen = ImageDataGenerator(dim_ordering="th", horizontal_flip=True, vertical_flip=True)
+# test_datagen = ImageDataGenerator(dim_ordering="th")  # dummy for testing to have the same structure
+#
+#
+# def chunk_generator(X_orig, y_orig, thickness=0, batch_size=32, is_training=True):
+#     while 1:
+#         logging.info("Loaded batch of patients with %d/%d positives" % (np.sum(y_orig), len(y_orig)))
+#         idx_sel = [i for i in range(len(X_orig)) if y_orig[i]==1 or random.uniform(0,1) < 1.2*np.mean(y_orig)]
+#         X = [X_orig[i] for i in idx_sel]
+#         y = [y_orig[i] for i in idx_sel]
+#         logging.info("Downsampled to %d/%d positives" % (np.sum(y), len(y)))
+#
+#         # convert to np array and add extra axis (needed for keras)
+#         X, y = np.asarray(X), np.asarray(y)
+#         y = np.expand_dims(y, axis=1)
+#         if thickness==0:
+#             X = np.expand_dims(X, axis=1)
+#
+#         # generator: if testing, do not augment data
+#         data_generator = train_datagen if is_training else test_datagen
+#
+#         i, good = 0, 0
+#         for X_batch, y_batch in data_generator.flow(X, y, batch_size=batch_size, shuffle=is_training):
+#             i += 1
+#             if good*batch_size > len(X)*2 or i>100:  # stop when we have augmented enough the batch
+#                 break
+#             if X_batch.shape[0] != batch_size:  # ensure correct batch size
+#                 continue
+#             good += 1
+#             yield X_batch, y_batch
 
 
-def chunk_generator(X_orig, y_orig, thickness=0, batch_size=32, is_training=True):
-    while 1:
-        logging.info("Loaded batch of patients with %d/%d positives" % (np.sum(y_orig), len(y_orig)))
-        idx_sel = [i for i in range(len(X_orig)) if y_orig[i]==1 or random.uniform(0,1) < 1.2*np.mean(y_orig)]
-        X = [X_orig[i] for i in idx_sel]
-        y = [y_orig[i] for i in idx_sel]
-        logging.info("Downsampled to %d/%d positives" % (np.sum(y), len(y)))
-
-        # convert to np array and add extra axis (needed for keras)
-        X, y = np.asarray(X), np.asarray(y)
-        y = np.expand_dims(y, axis=1)
-        if thickness==0:
-            X = np.expand_dims(X, axis=1)
-
-        # generator: if testing, do not augment data
-        data_generator = train_datagen if is_training else test_datagen
-
-        i, good = 0, 0
-        for X_batch, y_batch in data_generator.flow(X, y, batch_size=batch_size, shuffle=is_training):
-            i += 1
-            if good*batch_size > len(X)*2 or i>100:  # stop when we have augmented enough the batch
-                break
-            if X_batch.shape[0] != batch_size:  # ensure correct batch size
-                continue
-            good += 1
-            yield X_batch, y_batch
-
-
-# LOADING PATCHES FROM DISK
-logging.info("Loading training and test sets")
-x_train = np.load(os.path.join(PATCHES_PATH, 'x_train_dl2.npz'))['arr_0']
-y_train = np.load(os.path.join(PATCHES_PATH, 'y_train_dl2.npz'))['arr_0']
-x_test = np.load(os.path.join(PATCHES_PATH, 'x_test_dl2.npz'))['arr_0']
-y_test = np.load(os.path.join(PATCHES_PATH, 'y_test_dl2.npz'))['arr_0']
-logging.info("Training set (1s/total): %d/%d" % (sum(y_train),len(y_train)))
-logging.info("Test set (1s/total): %d/%d" % (sum(y_test), len(y_test)))
-
-# Load model
-model = ResnetBuilder().build_resnet_34((3,40,40),1)
-model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy','fmeasure'])
-model_checkpoint = ModelCheckpoint(OUTPUT_MODEL, monitor='loss', save_best_only=True)
-# logging.info('Loading exiting model...')
-# model.load_weights(OUTPUT_MODEL)
-
-
-model.fit_generator(generator=chunk_generator(x_train, y_train, batch_size=16, thickness=1),
-                    samples_per_epoch=1280,  # make it small to update TB and CHECKPOINT frequently
-                    nb_epoch=500,
-                    verbose=1,
-                    callbacks=[tb, model_checkpoint],
-                    validation_data=chunk_generator(x_test, y_test, batch_size=16, thickness=1, is_training=False),
-                    nb_val_samples=len(y_test),
-                    max_q_size=64,
-                    nb_worker=1)  # a locker is needed if increased the number of parallel workers
+# # LOADING PATCHES FROM DISK
+# logging.info("Loading training and test sets")
+# x_train = np.load(os.path.join(PATCHES_PATH, 'x_train_dl2.npz'))['arr_0']
+# y_train = np.load(os.path.join(PATCHES_PATH, 'y_train_dl2.npz'))['arr_0']
+# x_test = np.load(os.path.join(PATCHES_PATH, 'x_test_dl2.npz'))['arr_0']
+# y_test = np.load(os.path.join(PATCHES_PATH, 'y_test_dl2.npz'))['arr_0']
+# logging.info("Training set (1s/total): %d/%d" % (sum(y_train),len(y_train)))
+# logging.info("Test set (1s/total): %d/%d" % (sum(y_test), len(y_test)))
+#
+# # Load model
+# model = ResnetBuilder().build_resnet_34((3,40,40),1)
+# model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy','fmeasure'])
+# model_checkpoint = ModelCheckpoint(OUTPUT_MODEL, monitor='loss', save_best_only=True)
+# # logging.info('Loading exiting model...')
+# # model.load_weights(OUTPUT_MODEL)
+#
+#
+# model.fit_generator(generator=chunk_generator(x_train, y_train, batch_size=16, thickness=1),
+#                     samples_per_epoch=1280,  # make it small to update TB and CHECKPOINT frequently
+#                     nb_epoch=500,
+#                     verbose=1,
+#                     callbacks=[tb, model_checkpoint],
+#                     validation_data=chunk_generator(x_test, y_test, batch_size=16, thickness=1, is_training=False),
+#                     nb_val_samples=len(y_test),
+#                     max_q_size=64,
+#                     nb_worker=1)  # a locker is needed if increased the number of parallel workers
 
 # # check generator
 # for X,y in chunk_generator(filenames_train, nodules_df, batch_size=16):
