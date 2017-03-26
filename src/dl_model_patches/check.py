1739520ab6e7ad3567a049fd0ba2969ab2d6b71e
Martí Mayo Casademont <martimayo@gmail.com>
2017-03-26 12:21:48 +0200

    no vessels yet

--- src/dl_model_patches/check.py
+++ src/dl_model_patches/check.py
@@ -1,15 +1,21 @@
 import os
-from common import load_patient, add_stats
+import numpy as np
+from dl_model_patches.common import load_patient, add_stats
 ### Quality checks for ROIs detection
 wp = os.environ['LUNG_PATH']
-# INPUT_PATH = wp + 'data/preprocessed5_sample'
+INPUT_PATH = wp + 'data/preprocessed5_sample'
 # INPUT_PATH = wp + 'data/preprocessed5_sample_watershed'
-INPUT_PATH = wp + 'data/preprocessed5_sample_th2'
+# INPUT_PATH = wp + 'data/preprocessed5_sample_th2'
 file_list = [os.path.join(INPUT_PATH, fp) for fp in os.listdir(INPUT_PATH)]
 
 total_stats = {}
 for filename in file_list:
-    X, y, rois, stats = load_patient(filename, discard_empty_nodules=True, output_rois=True, thickness=0)
+    print "Loading patient: " + filename
+    patient_data = np.load(filename)['arr_0']
+    X, y, rois, stats = load_patient(patient_data=patient_data, patient_nodules_df=None,
+                                     discard_empty_nodules=True, output_rois=True,
+                                     debug=True, thickness=1)
     print stats
     total_stats = add_stats(stats, total_stats)
     print "TOTAL STATS:", filename.split('/')[-1], total_stats
+    break
