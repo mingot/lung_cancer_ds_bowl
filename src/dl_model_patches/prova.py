c104c76ceaaf32048b482e5345f1bab8cf7ccb06
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2017-03-29 17:28:16 +0200

    small fix

--- src/dl_model_patches/prova.py
+++ src/dl_model_patches/prova.py
@@ -2,17 +2,46 @@ import re
 import json
 import ast
 
-idx = 0
-with open('/Users/mingot/Projectes/kaggle/ds_bowl_lung/personal/debug_lung_segmentation/output_pre5.txt') as file:
-    print 'patientid,fp,fn,tp'
-    for row in file:
-        idx += 1
-        # if idx>10:break
-        xx = re.match('.* Patient: (.*), stats: (.*)', row)
-        if xx is not None:
-            j = ast.literal_eval(xx.group(2))
-            try:
-                print "%s, %s, %s, %s" % (xx.group(1), j['fp'], j['fn'], j['tp'])
-            except:
-                print "%s,,," % xx.group(1)
+# idx = 0
+# with open('/Users/mingot/Projectes/kaggle/ds_bowl_lung/personal/debug_lung_segmentation/output_pre5.txt') as file:
+#     print 'patientid,fp,fn,tp'
+#     for row in file:
+#         idx += 1
+#         # if idx>10:break
+#         xx = re.match('.* Patient: (.*), stats: (.*)', row)
+#         if xx is not None:
+#             j = ast.literal_eval(xx.group(2))
+#             try:
+#                 print "%s, %s, %s, %s" % (xx.group(1), j['fp'], j['fn'], j['tp'])
+#             except:
+#                 print "%s,,," % xx.group(1)
+
+
+import os
+
+INPUT_PATH = '/mnt/hd2/preprocessed5'  # INPUT_PATH = wp + 'data/preprocessed5_sample'
+VALIDATION_PATH = '/mnt/hd2/preprocessed5_validation_luna'
+
+
+f = open('/home/aitor/lung_cancer_ds_bowl/output/nodules_patches_dl1_v11.csv')
+
+file_list = [os.path.join(VALIDATION_PATH, fp) for fp in os.listdir(VALIDATION_PATH)]
+file_list += [os.path.join(INPUT_PATH, fp) for fp in os.listdir(INPUT_PATH)] # if fp.startswith('dsb_')]
+
+
+
+nslice_old, fidx = 0, 0
+pid = file_list[fidx]
+
+for idx,row in enumerate(f):
+    if idx>10:
+        break
+    row = row.split(',')
+    nslice = row[1]
+    if nslice>=nslice_old:
+        print "%s,%s" % (patientid,','.join(row[1:]))
+    else:
+        patientid = file_list[fidx]
+        nslice_old = nslice
+        print "%s,%s" % (patientid,','.join(row[1:]))
 
