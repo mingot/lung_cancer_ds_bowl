8a699b3eac83474889a2ff0b09d6953d999b6768
felix <felix.axel.gimeno@estudiant.upc.edu>
2017-04-05 22:10:45 +0000

    try fix

--- src/dl_model_simplepos_patches/evaluate.py
+++ src/dl_model_simplepos_patches/evaluate.py
@@ -6,8 +6,13 @@ import numpy as np
 import pandas as pd
 import multiprocessing
 from time import time
-sys.append('..')
+sys.path.append('..')
+#import dl_networks
+from dl_networks.simplepos_resnet import simpleposResnet
+from keras.optimizers import Adam
 from dl_model_patches import  common
+from dl_model_simplepos_patches import common
+from keras import backend as K
 
 wp = '/home/jose/lung_cancer_ds_bowl/'
 INPUT_PATH = '/mnt/hd2/preprocessed5'
@@ -21,50 +26,56 @@ def worker(filename, q):
     while 1:
         if q.qsize() < 10:
             patient_data = np.load(filename)['arr_0']
+            logging.info("[WORKER] "+str(filename)+" patient_data shape " +str(patient_data.shape)) 
             X, y, rois, stats = common.load_patient_3d_atlas(patient_data, discard_empty_nodules=True, output_rois=True, debug=True, thickness=1)
+            logging.info("[WORKER] len X " +str(len(X)))
             logging.info("Patient: %s, stats: %s" % (filename.split('/')[-1], stats))
             q.put((filename,X,y,rois))
             break
 
 def listener(q):
+    logging.info("[LISTENER] Started")
     """Reads regions from queue, predicts nodules and stores in the output file."""
-    from keras import backend as K
-    from dl_networks.sample_resnet import ResnetBuilder
-    from keras.optimizers import Adam
-
+    #from keras import backend as K
+    #from dl_networks.sample_resnet import ResnetBuilder
+    #from keras.optimizers import Adam
     # Model loading inside the listener thread (otherwise keras complains)
-    K.set_image_dim_ordering('th')
-    from dl_networks.simplepos_resnet import simpleposResnet
-    model = simpleposResnet().get_posResnet((3,40,40),5) # 5 == N_extra_features
-    model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy','fmeasure'])
-    model.load_weights(OUTPUT_MODEL)
-    logging.info('Loading existing model...')
+    try:    
+        K.set_image_dim_ordering('th')
+        #from dl_networks.simplepos_resnet import simpleposResnet
+        model = simpleposResnet().get_posResnet((3,40,40),(5,)) # 5 == N_extra_features
+        model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy','fmeasure'])
+        model.load_weights(OUTPUT_MODEL)
+        logging.info('Loading existing model...')
 
-    total, errors = 0, 0
-    csvfile = open(OUTPUT_CSV, 'w')
-    writer = csv.DictWriter(csvfile, fieldnames=['patientid','min','mean','max','std'])
-    writer.writeheader()
-    while 1:
-        m = q.get()
-        if m == 'kill':
-            logging.info('[LISTENER] Closing...')
-            break
+        total, errors = 0, 0
+        csvfile = open(OUTPUT_CSV, 'w')
+        writer = csv.DictWriter(csvfile, fieldnames=['patientid','min','mean','max','std'])
+        writer.writeheader()
+        while 1:
+            m = q.get()
+            if m == 'kill':
+                logging.info('[LISTENER] Closing...')
+                break
 
-        try:
-            filename, x, _, _ = m
-            filename = filename.split('/')[-1]
+            try:
+                filename, x, _, _ = m
+                filename = filename.split('/')[-1]
+                if 0 == len(x): continue
+                print x #np.asarray(x).shape
+                preds = model.predict(np.asarray(x), verbose=1)
+                logging.info("[LISTENER] Predicted patient %d %s" % (total, filename))
+                writer.writerow({'patientid':filename,'min':preds.min(),'max':preds.max(),'std':preds.std(),'mean':preds.mean()})
+                total += 1
+            except Exception as e:
+                logging.error("[LISTENER] Error processing result, skipping. %s" % str(e))
+                errors += 1
 
-            preds = model.predict(np.asarray(x), verbose=1)
-            logging.info("[LISTENER] Predicted patient %d %s" % (total, filename))
-            writer.writerow({'patientid':filename,'min':preds.min(),'max':preds.max(),'std':preds.std(),'mean':preds.mean()})
-            total += 1
-        except Exception as e:
-            logging.error("[LISTENER] Error processing result, skipping. %s" % str(e))
-            errors += 1
-
-    logging.info("Stats: %d patients, %d errors" % (total,errors))
-    csvfile.close()
-
+        logging.info("Stats: %d patients, %d errors" % (total,errors))
+        csvfile.close()
+    except Exception as e:
+        logging.info("[LISTENER] Exception "+str(e) )
+    logging.info("[LISTENER] Closing")
 
 def main():
     manager = multiprocessing.Manager()
@@ -79,6 +90,7 @@ def main():
     for filename in file_list:
         job = pool.apply_async(worker, (filename, q))
         jobs.append(job)
+        #break
 
     # collect results from the workers through the pool result queue
     for job in jobs:
