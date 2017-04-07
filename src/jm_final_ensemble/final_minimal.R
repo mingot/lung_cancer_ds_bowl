9fb41a03d4129b169ba5cc9a3e9b421e023b7fef
mingot <jm.mingot@gmail.com>
2017-04-07 13:01:15 +0000

    modified final_minimal to include sergi features

--- src/jm_final_ensemble/final_minimal.R
+++ src/jm_final_ensemble/final_minimal.R
@@ -1,6 +1,7 @@
 library(data.table)
 library(pROC)
 library(dplyr)
+library(LiblineaR)
 
 # INITIAL SETUP -----------------------------------------------------------------
 
@@ -50,14 +51,22 @@ dl12_final = dl12[,.(max_diameter_patches = max(diameter, na.rm=T),
 df_nodule = fread(NODULES_EXTENDED_FILE)
 df_nodule = plyr::ddply(
   df_nodule, "patientid", 
-  function(df)  {
-    df_top = dplyr::arrange(df, desc(score)) %>% head(1) %>% as.data.table
-    df_out = df_top[, !"patientid"]
-    df_out$man_nslicespread_sd = sd(df$nslicesSpread)
-    df_out
+  function(df) {
+    df_max = df[which.max(df$score), ]
+    df_max$patientid = NULL
+    df_max$nslicespread_sd = sd(df$nslicesSpread) 
+    names(df_max) = paste0("maxscorenod_", names(df_max))
+
+    df_maxd = df[which.max(df$diameter), ]
+    df_maxd$patientid = NULL
+    names(df_maxd) = paste0("maxdiamnod_", names(df_maxd))
+
+    data.table(df_max, df_maxd, stringsAsFactors = FALSE)
   }, .progress = "text"
 ) %>% as.data.table
-df_nodule = df_nodule[,.(patientid,PC1_lbp, man_nslicespread_sd)]
+df_nodule = df_nodule[,.(patientid, maxscorenod_nslicespread_sd, maxscorenod_PC1_lbp, maxscorenod_10_lungmask, maxscorenod_y, maxscorenod_PC3_lbp,
+                         maxdiamnod_PC2_hog, maxdiamnod_x, maxdiamnod_40_nodeverticalposition)]
+
 
 
 # DL3 features (just used for model 02)
@@ -66,43 +75,45 @@ dl3_df = dl3_df[,.(dl3_max=max(score),
                    dl3_has=as.numeric(sum(score>0.7)>0),
                    dl3_nohas=as.numeric(sum(score<.3)==.N)),by=patientid]
 
+
 # model 01 TRAIN -------------------------------------------------------------------
 
-# # create data train df
-# data_train = fread(ANNOTATIONS_FILE)
-# setnames(data_train,'id','patientid')
-# data_train[,patientid:=paste0("dsb_",patientid,".npz")]
-# data_train = merge(data_train, dl12_final, by='patientid', all.x=T)
-# data_train = merge(data_train, df_nodule, by='patientid', all.x=T)  # TODO: check if we are getting all the filenames
-# data_train[is.na(data_train)] = 0
-# 
-# vars_sel = c('max_diameter_patches','max_score_patches','PC1_lbp','man_nslicespread_sd')
-# 
-# aucs = c(); lls = c()
-# for(i in 1:20){
-#   k = 3; data_train$id = sample(1:k, nrow(data_train), replace = TRUE); list = 1:k
-#   for (i in 1:k){
-#     trainingset = subset(data_train, id %in% list[-i])
-#     testset = subset(data_train, id %in% c(i))
-#     
-#     # train
-#     mymodel = glm(cancer ~ 1 + ., family=binomial(link='logit'), data=trainingset[,c("cancer",vars_sel),with=F])
-#     
-#     # test
-#     pred = predict(mymodel, testset[,c("cancer",vars_sel),with=F], type="response")
-#     real = testset$cancer
-#     
-#     # store results
-#     aucs = c(aucs, auc(real,pred))
-#     lls = c(lls, MultiLogLoss(real,pred))
-#   }
-# }
-# summary(mymodel); summary(aucs); summary(lls)
-# 
-# # store final model
-# final_model_01 = glm(cancer ~ 1 + ., family=binomial(link='logit'), data=data_train[,c("cancer",vars_sel),with=F])
-# summary(final_model_01)
-# save(final_model_01, file=FINAL_MODEL_01)
+# create data train df
+data_train = fread(ANNOTATIONS_FILE)
+setnames(data_train,'id','patientid')
+data_train[,patientid:=paste0("dsb_",patientid,".npz")]
+data_train = merge(data_train, dl12_final, by='patientid', all.x=T)
+data_train = merge(data_train, df_nodule, by='patientid', all.x=T)  # TODO: check if we are getting all the filenames
+data_train[is.na(data_train)] = 0
+
+vars_sel = c('max_diameter_patches','max_score_patches','maxscorenod_PC1_lbp', 'maxscorenod_nslicespread_sd',
+             "maxscorenod_10_lungmask",'maxscorenod_y', "maxscorenod_PC3_lbp","maxdiamnod_40_nodeverticalposition", "maxdiamnod_x","maxdiamnod_PC2_hog")
+
+aucs = c(); lls = c()
+for(i in 1:50){
+  k = 3; data_train$id = sample(1:k, nrow(data_train), replace = TRUE); list = 1:k
+  for (i in 1:k){
+    trainingset = subset(data_train, id %in% list[-i])
+    testset = subset(data_train, id %in% c(i))
+
+    # train
+    mymodel = glm(cancer ~ 1 + ., family=binomial(link='logit'), data=trainingset[,c("cancer",vars_sel),with=F])
+
+    # test
+    pred = predict(mymodel, testset[,c("cancer",vars_sel),with=F], type="response")
+    real = testset$cancer
+
+    # store results
+    aucs = c(aucs, auc(real,pred))
+    lls = c(lls, MultiLogLoss(real,pred))
+  }
+}
+summary(mymodel); summary(aucs); summary(lls)
+
+# store final model
+final_model_01 = glm(cancer ~ 1 + ., family=binomial(link='logit'), data=data_train[,c("cancer",vars_sel),with=F])
+summary(final_model_01)
+save(final_model_01, file=FINAL_MODEL_01)
 
 # model 01 EXECUTION ------------------------------------------------------
 
@@ -116,11 +127,14 @@ data_test[is.na(data_test)] = 0
 
 
 load(FINAL_MODEL_01)
-vars_sel01 = c('max_diameter_patches','max_score_patches','PC1_lbp','man_nslicespread_sd')
+vars_sel01 = c('max_diameter_patches','max_score_patches','maxscorenod_PC1_lbp', 'maxscorenod_nslicespread_sd',
+             "maxscorenod_10_lungmask",'maxscorenod_y', "maxscorenod_PC3_lbp","maxdiamnod_40_nodeverticalposition", "maxdiamnod_x","maxdiamnod_PC2_hog")
 preds01 = predict(final_model_01, data_test[,vars_sel01,with=F], type="response")
 submission01 = data.table(id=gsub(".npz|dsb_","",data_test$patientid), cancer=preds01)
 cat("Mean cancer in submission01:", mean(submission01$cancer),"\n")
 write.csv(submission01, SUBMISSION_OUTPUT01, quote=F, row.names=F)
+write.csv(submission01, "/home/shared/output/submissions/final_submission_01_v02.csv", quote=F, row.names=F)
+
   
 # model 02 TRAIN ----------------------------------------------------------------
 
@@ -138,7 +152,7 @@ write.csv(submission01, SUBMISSION_OUTPUT01, quote=F, row.names=F)
 # data_train[is.na(data_train)] = 0  # replace NA's with 0's
 # 
 # data_train = data_train[patientid%in%validation$patientid]
-# vars_sel = c('max_diameter_patches','PC1_lbp','man_nslicespread_sd','dl3_has')
+# vars_sel = c('max_diameter_patches','maxscorenod_PC1_lbp','maxscorenod_nslicespread_sd','dl3_has')
 # 
 # aucs = c(); lls = c()
 # for(i in 1:50){
@@ -179,7 +193,7 @@ data_test[is.na(dl3_nohas), dl3_nohas:=1]
 data_test[is.na(data_test)] = 0
 
 load(FINAL_MODEL_02)
-vars_sel02 = c('max_diameter_patches','PC1_lbp','man_nslicespread_sd','dl3_has')
+vars_sel02 = c('max_diameter_patches','maxscorenod_PC1_lbp','maxscorenod_nslicespread_sd','dl3_has')
 preds02 = predict(final_model_02, data_test[,vars_sel02,with=F], type="response")
 submission02 = data.table(id=gsub(".npz|dsb_","",data_test$patientid), cancer=preds02)
 cat("Mean cancer in submission02:", mean(submission02$cancer),"\n")
