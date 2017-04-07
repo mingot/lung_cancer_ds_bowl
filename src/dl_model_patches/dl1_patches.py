321791de41d74a2d714d6f43d31b49c829b9625a
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2017-04-07 07:44:33 +0200

    DL1 v19 train

--- src/dl_model_patches/dl1_patches.py
+++ src/dl_model_patches/dl1_patches.py
@@ -135,8 +135,8 @@ logging.info("Test set (1s/total): %d/%d" % (sum(y_test), len(y_test)))
 # Load model
 model = ResnetBuilder().build_resnet_50((3,40,40),1)
 model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy','fmeasure'])
-# logging.info('Loading exiting model...')
-# model.load_weights(OUTPUT_MODEL)
+logging.info('Loading exiting model...')
+model.load_weights(OUTPUT_MODEL)
 
 
 model.fit_generator(generator=chunks(x_train, y_train, batch_size=32, thickness=1),
@@ -148,7 +148,7 @@ model.fit_generator(generator=chunks(x_train, y_train, batch_size=32, thickness=
                     validation_data=chunks(x_test, y_test, batch_size=32, thickness=1, is_training=False,),
                     nb_val_samples=32*40,
                     max_q_size=10,
-                    #initial_epoch=1120,
+                    initial_epoch=715,
                     nb_worker=1)  # a locker is needed if increased the number of parallel workers
 
 #
